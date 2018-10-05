2fd87c842b508303381f5f544e16a93b0c36bd67
Jimmy Svensson <jimmy888swe@gmail.com>
2018-10-05 11:03:35 +0200

    sorterat och medelande om vinnare

--- galopp.js
+++ galopp.js
@@ -22,6 +22,7 @@ krav: * hästen rör sig 1,2,3 steg
 
 // en funktion (comstructor) som är till för att skapa hästar.
 var leaderPosition = 0;
+const distance = 20;
 
 function RaceHorses(number) {
     this.number = number;
@@ -41,17 +42,20 @@ var horse4 = new RaceHorses(4);
 var allHorses = [
     horse1, horse2, horse3, horse4
 ];
+// slumpa fram poöng till hästarna
+function moveHorses(){
+allHorses.forEach(function(horse){
+    let slump = Math.floor(Math.random() * 3 );
+horse.position += slump;
+});
 
+}
 // Kontrollera om någon vunnit, samt loopar spelet
-while ( leaderPosition < 20){
+while ( leaderPosition < distance){
 
 // Rulla bollarna i hål.
 
-horse1.position +=1;
-horse2.position +=3;
-horse3.position +=1;
-horse4.position +=2;
-
+moveHorses();
 
 // skriver hur hästarna ligger till
 
@@ -59,9 +63,16 @@ allHorses.forEach(function(horse){
     console.log("Häst " + horse.number + ":" + horse.position + "\n");
     if (horse.position > leaderPosition){
         leaderPosition = horse.position;
+        
     }
   });
-
 }
+// sortera hästarna efter leaderPosition
 
+allHorses.sort(function(a,b){
+if (a.position < b.position) return 1;
+if (a.position > b.position) return -1;
+});
+console.log(allHorses);
 
+console.log('The winner is ' + allHorses[0].number +  '!');
